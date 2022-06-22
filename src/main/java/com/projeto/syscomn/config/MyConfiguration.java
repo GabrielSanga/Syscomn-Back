eb6368265b8b09cf75cb7d50b38734ef13959abb
Gabriel Sanga <gabriel.sanga.fef@gmail.com>
2022-06-22 19:42:16 -0300

    Ajustado a permissão do CORS

--- src/main/java/com/projeto/syscomn/config/MyConfiguration.java
+++ src/main/java/com/projeto/syscomn/config/MyConfiguration.java
@@ -10,7 +10,7 @@ public class MyConfiguration implements WebMvcConfigurer {
 	  @Override
 	    public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	            .allowedOrigins("http://localhost:4200")
+	            .allowedOrigins("https://syscomn.herokuapp.com/")
 	            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 	    }
 	  
