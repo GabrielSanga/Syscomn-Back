ead909c20ce2e64ee5cff144df459b20265febb1
Gabriel Sanga <gabriel.sanga.fef@gmail.com>
2022-06-22 19:49:30 -0300

    CORS

--- src/main/java/com/projeto/syscomn/config/MyConfiguration.java
+++ src/main/java/com/projeto/syscomn/config/MyConfiguration.java
@@ -10,7 +10,7 @@ public class MyConfiguration implements WebMvcConfigurer {
 	  @Override
 	    public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	            .allowedOrigins("https://syscomn.herokuapp.com")
+	            .allowedOrigins("http://localhost:4200")
 	            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 	    }
 	  
--- src/main/java/com/projeto/syscomn/resources/AdministradorResource.java
+++ src/main/java/com/projeto/syscomn/resources/AdministradorResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.projeto.syscomn.domain.Administrador;
 import com.projeto.syscomn.domain.dtos.AdministradorDTO;
 import com.projeto.syscomn.services.AdministradorService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "administrador")
 public class AdministradorResource {
